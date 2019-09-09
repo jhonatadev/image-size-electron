9fd9df383c39e06c9bfd0858b5b44216a3960af3
jhonatadev <xxxonata@gmail.com>
2019-09-08 22:07:20 -0300

    Definindo responsabilidades

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const electron = require('electron')
 const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-  const janelaPrincipal = new BrowserWindow({})
+  const janelaPrincipal = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
   janelaPrincipal.loadURL(`file://${__dirname}/index.html`)
 })
