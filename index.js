9fe8d7fa9c1cdeea57fe557cbc345bad1173fdcc
jhonatadev <xxxonata@gmail.com>
2019-09-08 22:27:18 -0300

    Usando modulo image-size

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require('electron')
-
+const sizeOf = require('image-size')
 const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
@@ -11,4 +11,8 @@ app.on('ready', () => {
   janelaPrincipal.loadURL(`file://${__dirname}/index.html`)
 })
 
-ipcMain.on('obterDimensoesDaImagem', () => {})
+ipcMain.on('obterDimensoesDaImagem', (e, path) => {
+	sizeOf(path, function(err, dimensions) {
+		console.log(`largura: ${dimensions.width}, Altura: ${dimensions.height}`)
+	})
+})
